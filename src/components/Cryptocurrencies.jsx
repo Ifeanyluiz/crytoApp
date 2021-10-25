1f649ad44b2f864bf3efd5f726db4a0244969e0e
Ifeanyi Onuoha <onuohaifeanyi2021@gmail.com>
2021-10-25 11:11:18 +0100

    Day1 build: packages installations  navbar, menu, body, footer

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoCurrencies = () => {
+    return (
+        <div>
+            Cryptocurrencies
+        </div>
+    )
+}
+
+export default CryptoCurrencies
